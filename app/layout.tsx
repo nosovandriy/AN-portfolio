2c1a735b5859c6e4b0b85d892b8da2cb39233e52
Andriy Nosov <nosovandriy@gmail.com>
2023-03-02 21:18:14 +0200

    meta tag fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { NavBar } from '@nav-bar/nav-bar';
 import { Footer } from '@footer/footer';
 
 export const metadata = {
+  title: 'AN | Front-End Developer',
+  description: 'Andriy Nosov portfolio | Front-End Developer',
+  
   openGraph: {
     title: 'AN | Front-End Developer',
     description: 'Andriy Nosov portfolio | Front-End Developer',
@@ -11,14 +14,6 @@ export const metadata = {
     images: [
       {
         url: 'https://serving.photos.photobox.com/82171614081d51b28f81e8d1274941ead3440a789a2607a9ef4f78c2fbd5dbefc99ffb93.jpg',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://serving.photos.photobox.com/82171614081d51b28f81e8d1274941ead3440a789a2607a9ef4f78c2fbd5dbefc99ffb93.jpg',
-        width: 1800,
-        height: 1600,
-        alt: 'andriy nosov portfolio',
       },
     ],
     locale: 'en-US',
