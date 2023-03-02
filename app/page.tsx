9efbea426590f39fd5a93cb7d00caeb9700af105
Andriy Nosov <nosovandriy@gmail.com>
2023-03-02 18:50:04 +0200

    meta tag add

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,21 @@ export default function Home() {
     <div className={raleway.className}>
       <Head>
         <meta charSet="utf-8" />
-        <meta property="og:title" content="AN | Web-Developer" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.andriynosov.com" />
-        <meta property="og:image" content="https://serving.photos.photobox.com/82171614081d51b28f81e8d1274941ead3440a789a2607a9ef4f78c2fbd5dbefc99ffb93.jpg" />
-        <meta name="google-site-verification" content="eMJqgXk3xiGY23OyAi6E4tf3ZzlJ-JNqbagb1loFB0o" />
-
         <title>AN | Web-Developer</title>
-        <meta name="description" content="AN | Web-Developer - Andriy Nosov portfolio" />
+        <meta name="description" content="Andriy Nosov portfolio" />
+
+        <meta property="og:url" content="https://andriynosov.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="AN | Web-Developer" />
+        <meta property="og:description" content="Andriy Nosov portfolio" />
+        <meta property="og:image" content="https://serving.photos.photobox.com/82171614081d51b28f81e8d1274941ead3440a789a2607a9ef4f78c2fbd5dbefc99ffb93.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="andriynosov.com" />
+        <meta property="twitter:url" content="https://andriynosov.com" />
+        <meta name="twitter:title" content="AN | Web-Developer" />
+        <meta name="twitter:description" content="Andriy Nosov portfolio" />
+        <meta name="twitter:image" content="" />
       </Head>
       <Main />
       <Resume />
