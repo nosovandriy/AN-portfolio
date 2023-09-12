529723fd12a0b92e1fe4716d040d225321dd02e4
Andriy Nosov <nosovandriy@gmail.com>
2023-09-12 10:50:19 +0300

    update-cv

--- app/components/skills/hook/useSkills.ts
+++ app/components/skills/hook/useSkills.ts
@@ -4,92 +4,89 @@ interface Skill {
   alt: string;
 }
 
-
-export function useSkills()
-: {
+export function useSkills(): {
   skills: Skill[];
-}
- {
+} {
   const skills = [
     {
-      title: 'HTML',
-      link: '/assets/skills/html.png',
-      alt: 'HTML logo',
+      title: "HTML",
+      link: "/assets/skills/html.png",
+      alt: "HTML logo",
     },
     {
-      title: 'CSS',
-      link: '/assets/skills/css.png',
-      alt: 'CSS logo',
+      title: "CSS",
+      link: "/assets/skills/css.png",
+      alt: "CSS logo",
     },
     {
-      title: 'JavaScript',
-      link: '/assets/skills/javascript.png',
-      alt: 'JavaScript logo',
+      title: "JavaScript",
+      link: "/assets/skills/javascript.png",
+      alt: "JavaScript logo",
     },
     {
-      title: 'TypeScript',
-      link: '/assets/skills/ts.png',
-      alt: 'TypeScript logo',
+      title: "TypeScript",
+      link: "/assets/skills/ts.png",
+      alt: "TypeScript logo",
     },
     {
-      title: 'React',
-      link: '/assets/skills/react.png',
-      alt: 'React logo',
+      title: "React",
+      link: "/assets/skills/react.png",
+      alt: "React logo",
     },
     {
-      title: 'NextJS',
-      link: '/assets/skills/nextjs.png',
-      alt: 'NextJS logo',
+      title: "React Native",
+      link: "/assets/skills/react-native.png",
+      alt: "React Native logo",
     },
     {
-      title: 'Tailwind',
-      link: '/assets/skills/tailwind.png',
-      alt: 'Tailwind logo',
+      title: "NextJS",
+      link: "/assets/skills/nextjs.png",
+      alt: "NextJS logo",
     },
     {
-      title: 'MUI',
-      link: '/assets/skills/mui.png',
-      alt: 'MUI logo',
+      title: "Redux",
+      link: "/assets/skills/redux.png",
+      alt: "Redux logo",
     },
     {
-      title: 'Bootstrap',
-      link: '/assets/skills/bootstrap.png',
-      alt: 'Bootstrap logo',
+      title: "Tailwind",
+      link: "/assets/skills/tailwind.png",
+      alt: "Tailwind logo",
     },
     {
-      title: 'SASS',
-      link: '/assets/skills/sass.png',
-      alt: 'SASS logo',
+      title: "MUI",
+      link: "/assets/skills/mui.png",
+      alt: "MUI logo",
     },
     {
-      title: 'Redux',
-      link: '/assets/skills/redux.png',
-      alt: 'Redux logo',
+      title: "Bootstrap",
+      link: "/assets/skills/bootstrap.png",
+      alt: "Bootstrap logo",
     },
     {
-      title: 'GitHub',
-      link: '/assets/skills/github.png',
-      alt: 'GitHub logo',
+      title: "SASS",
+      link: "/assets/skills/sass.png",
+      alt: "SASS logo",
     },
     {
-      title: 'RTL',
-      link: '/assets/skills/react-testing-library.png',
-      alt: 'React Testing Library logo',
+      title: "GitHub",
+      link: "/assets/skills/github.png",
+      alt: "GitHub logo",
     },
     {
-      title: 'Postman',
-      link: '/assets/skills/postman.png',
-      alt: 'Postman logo',
+      title: "Postman",
+      link: "/assets/skills/postman.png",
+      alt: "Postman logo",
     },
     {
-      title: 'Figma',
-      link: '/assets/skills/figma.png',
-      alt: 'Figma logo',
+      title: "Figma",
+      link: "/assets/skills/figma.png",
+      alt: "Figma logo",
     },
     {
-      title: 'Git',
-      link: '/assets/skills/git.png',
-      alt: 'Git logo',
+      title: "Git",
+      link: "/assets/skills/git.png",
+      alt: "Git logo",
     },
   ];
 
